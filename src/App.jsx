13c363edd909f2616f7eb6118ac52fc715f732ed
SAISHANKER R <www.rsaishanker@gmail.com>
2026-03-28 23:28:13 +0530

    Interactive storytelling hyperloop website

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import HyperloopStory from './HyperloopStory'
+import './index.css' // We might not need this if not existing, but vite creates it. We will keep what we need.
+
+function App() {
+  return (
+    <>
+      <HyperloopStory />
+    </>
+  )
+}
+
+export default App
